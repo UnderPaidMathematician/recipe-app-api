ec7785a33c03df9ff7fb8fc4ff51d8253c9a09cd
UnderPaidMathematician <61880854+UnderPaidMathematician@users.noreply.github.com>
2024-01-07 11:37:21 -0500

    before adding remote-ptb

--- app/user/tests/tests_user_api.py
+++ app/user/tests/tests_user_api.py
@@ -1,5 +1,5 @@
 """
-Tests for the user api.
+Tests for the user API.
 """
 from django.test import TestCase
 from django.contrib.auth import get_user_model
@@ -46,13 +46,15 @@ class PublicUserApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_password_too_short_error(self):
-        """Test an error is ruturned if password is less than 5 characters."""
+        """Test an error is returned if password is less than 5 characters."""
         payload = {
             'email': 'test@example.com',
             'password': 'pw',
-            'name': 'Test Name'
+            'name': 'Test name',
         }
         res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
             email=payload['email']
         ).exists()
