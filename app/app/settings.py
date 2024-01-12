1fc73e5f89fdb6a23416130be16b47b5409ba2a7
UnderPaidMathematician <61880854+UnderPaidMathematician@users.noreply.github.com>
2024-01-12 16:22:04 -0500

    Implemented swagger as well as token authentication

--- app/app/settings.py
+++ app/app/settings.py
@@ -28,8 +28,6 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
-# Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,10 +36,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'drf_spectacular',
     'core',
     'user',
 ]
+# Application definition
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,6 +1,10 @@
 """
 Serializers for the user api view."""
-from django.contrib.auth import get_user_model
+from django.contrib.auth import (
+    get_user_model,
+    authenticate,
+)
+from django.utils.translation import gettext_lazy as _
 
 from rest_framework import serializers
 
@@ -15,3 +19,38 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Create and return a user with encrypted password"""
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update and return user"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serilizer for the user authentication token."""
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+    )
+
+    def validate(self, atters):
+        """Validate and authenticate the user."""
+        email = atters.get('email')
+        password = atters.get('password')
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password,
+        )
+        if not user:
+            msg = _('Unable to authenticate with provide credentials.')
+            raise serializers.ValidationError(msg, code='authorization')
+
+        atters['user'] = user
+        return atters
